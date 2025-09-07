68e1b117da43502024c7eab6540846846a1c3af4
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2025-09-07 12:14:55 -0300

    🚀 Initial commit: Setup iOS project

---
+++ SeedsProsperity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeedsProsperity
+//
+//  Created by Alexandre Cardoso on 07/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
